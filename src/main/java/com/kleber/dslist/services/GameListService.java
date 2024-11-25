1b12f98ad719aa8bd953377ec5baf2718e6dc6e3
kleberton-santos <ti.klebersantos@gmail.com>
2024-11-25 17:12:07 -0300

    Move games on list

--- src/main/java/com/kleber/dslist/services/GameListService.java
+++ src/main/java/com/kleber/dslist/services/GameListService.java
@@ -10,6 +10,8 @@ import com.kleber.dslist.entities.GameList;
 import com.kleber.dslist.entities.dto.GameListDTO;
 import com.kleber.dslist.entities.dto.GameMinDTO;
 import com.kleber.dslist.entities.repositories.GameListRepository;
+import com.kleber.dslist.entities.repositories.GameRepository;
+import com.kleber.dslist.projection.GameMinProjection;
 
 
 @Service
@@ -18,11 +20,30 @@ public class GameListService {
 	@Autowired
 	private GameListRepository gameListRepository;
 	
+	@Autowired
+	private GameRepository gameRepository;
+	
 	@Transactional(readOnly = true )
 	public List<GameListDTO> findAll(){
 		List<GameList> result = gameListRepository.findAll();
 		return result.stream().map(x -> new GameListDTO(x)).toList();
 	
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+		
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+		
 	}
 
 }
