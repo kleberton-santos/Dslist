d74b68cc6eb49d7d84bddb46c187e7c326b2b308
kleberton-santos <ti.klebersantos@gmail.com>
2024-11-25 11:35:11 -0300

    GET game by id

--- src/main/java/com/kleber/dslist/services/GameService.java
+++ src/main/java/com/kleber/dslist/services/GameService.java
@@ -4,9 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.kleber.dslist.entities.Game;
+import com.kleber.dslist.entities.GameDTO;
 import com.kleber.dslist.entities.dto.GameMinDTO;
 import com.kleber.dslist.entities.repositories.GameRepository;
+
 
 @Service
 public class GameService {
@@ -14,6 +18,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true )
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true )
 	public List<GameMinDTO> findAll(){
 		var result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
